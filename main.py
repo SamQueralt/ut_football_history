7156c1aa9a41e76f83fb81ee073bb81c58919a05
SamQueralt <squeralt@utexas.edu>
2024-08-07 21:56:42 -0500

    expanded sidebar by default

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(
     page_title = "UT Football Box Score History",
     page_icon = "🤘",
     layout = "wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
 )
 
 master_offense = pd.read_csv('offensive_stats_final.csv').sort_values(by='Date')
